549de66f20e781f1050ba0aa74bb9dea351783cc
Loc Huynh <huynhloc.contact@gmail.com>
2026-03-16 10:50:59 +0700

    clang-format

--- src/lotus-state.cpp
+++ src/lotus-state.cpp
@@ -1068,7 +1068,7 @@ namespace fcitx {
         }
 
         std::string_view lookback(text.data(), cursor);
-        size_t      lastNonSpace = lookback.find_last_not_of(" \t\r\n");
+        size_t           lastNonSpace = lookback.find_last_not_of(" \t\r\n");
         if (lastNonSpace == std::string::npos) {
             return true;
         }
@@ -1081,7 +1081,7 @@ namespace fcitx {
             } else {
                 wordStart++;
             }
-            std::string_view word(&lookback[wordStart], lastNonSpace - wordStart + 1);
+            std::string_view                                  word(&lookback[wordStart], lastNonSpace - wordStart + 1);
 
             static const std::unordered_set<std::string_view> abbreviations = {"Mr.", "Ms.", "Dr.", "etc.", "vs.", "Mrs.", "Prof.", "St.", "Sr.", "Jr."};
             if (abbreviations.count(word)) {
