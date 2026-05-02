aab40417f7af59700fd54b787647fffefe3d4e59
loccun <huynhloc.contact@gmail.com>
2026-05-02 16:12:23 +0700

    chore: update bamboo-core to clean BracketTransform mode
    - Remove unnecessary comments
    - Clean code (no comments needed if logic is clear)
    - Point to commit f024aa6

--- src/lotus-config.h
+++ src/lotus-config.h
@@ -48,12 +48,23 @@ namespace fcitx {
      * @brief W2U mode for w to ư conversion.
      */
     enum class W2UMode : std::uint8_t {
-        Disabled   = 0,
+        Disabled   =0,
         NonStart   = 1,
         Everywhere = 2,
     };
 
     FCITX_CONFIG_ENUM_NAME_WITH_I18N(W2UMode, N_("Disabled"), N_("Non-Start"), N_("Everywhere"));
+
+    /**
+     * @brief Bracket transform mode for [ -> ơ, ] -> ư conversion.
+     */
+    enum class BracketTransformMode : std::uint8_t {
+        Disabled   =0,
+        NonStart   = 1,
+        Everywhere = 2,
+    };
+
+    FCITX_CONFIG_ENUM_NAME_WITH_I18N(BracketTransformMode, N_("Disabled"), N_("Non-Start"), N_("Everywhere"));
 
     /**
      * @brief Icon theme options.
@@ -200,7 +211,7 @@ namespace fcitx {
             this, "ModeMenuKey", _("Mode Menu Hotkey"), {Key("grave")}, KeyListConstrain({KeyConstrainFlag::AllowModifierLess, KeyConstrainFlag::AllowModifierOnly})};
         SubConfigOption                                      appRules{this, "AppRules", _("App Rules"), "fcitx://config/addon/lotus/app_rules"};
         OptionWithAnnotation<W2UMode, W2UModeI18NAnnotation> w2u{this, "W2U", _("Type w to Produce ư"), W2UMode::NonStart};
-        Option<bool> bracketTransform{this, "BracketTransform", _("Enable [ -> ơ, ] -> ư (Works with any input method)"), false};
+        OptionWithAnnotation<BracketTransformMode, BracketTransformModeI18NAnnotation> bracketTransform{this, "BracketTransform", _("Enable [ -> ơ, ] -> ư (Works with any input method)"), BracketTransformMode::NonStart};
 
         Option<bool> spellCheck{this, "SpellCheck", _("Enable Spell Check"), true}; Option<bool> enableMacro{this, "EnableMacro", _("Enable Macro"), true};
         Option<bool> capitalizeMacro{this, "CapitalizeMacro", _("Capitalize Macro"), true}; Option<bool> autoCapitalizeAfterPunctuation{
