1fa85270c80a76665a66ea335c2d55ba2389e53b
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2026-09-10 18:20:58 +0700

    update hash of fcitx5-lotus

--- nix/packages/fcitx5-lotus/default.nix
+++ nix/packages/fcitx5-lotus/default.nix
@@ -36,7 +36,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "LotusInputMethod";
     repo = "fcitx5-lotus";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-kOIs8nLSF93xDIU7v8Wldyw+Zs5NEMJqZA/42TN4oYM=";
     fetchSubmodules = true;
   };
 
